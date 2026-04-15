2f76279b1aacfe83b7a7c275c432947fe4c57a2a
Razvan Turcanu <razvan.turcanu@visma.com>
2026-04-15 16:44:18 +0300

    added diacritics to setari website

--- schemaTypes/general-info.ts
+++ schemaTypes/general-info.ts
@@ -3,7 +3,7 @@ import { defineField } from "sanity";
 
 const generalInfo = {
   name: "general-info",
-  title: "Setari website",
+  title: "Setări website",
   type: "document",
   __experimental_actions: ['update', 'publish'], // disables create + delete
 
@@ -26,17 +26,17 @@ const generalInfo = {
 
       defineField({
       name: 'cityPageCover',
-      title: 'Coperta Pagina Sibiu/Valcea',
+      title: 'Coperta Pagină Sibiu/Vâlcea',
       type: "duoImage",
       
       }),
 
       {
         name: 'currentYearImage',
-        title: 'Imagine editie curenta',
+        title: 'Imagine ediție curentă',
         type: 'image',
         options: { hotspot: true },
-        description:"dimensiuniune recomandata: max 800x800 ~30kb"
+        description:"dimensiune recomandată: max 800x800 ~30kb"
       },
 
       defineField({
@@ -48,21 +48,21 @@ const generalInfo = {
 
     {
       name: 'misionStatement1',
-      title: 'Misiune (coloana 1)',
+      title: 'Misiune (coloană 1)',
       type: "array",
       of: [{type: "block"}],
     },
 
     {
       name: 'misionStatement2',
-      title: 'Misiune (coloana 2)',
+      title: 'Misiune (coloană 2)',
       type: "array",
       of: [{type: "block"}],
     },
 
     {
       name: 'contactFormImage',
-      title: 'Imagine Devino Gazda',
+      title: 'Imagine Devino Gazdă',
       type: 'image',
       options: { hotspot: true },
       hidden: true,
@@ -80,21 +80,21 @@ const generalInfo = {
       name: "mediaKitSibiu",
       type: "file",
       title: "Media Kit Sibiu",
-      description: "Un singur fisier .zip"
+      description: "Un singur fișier .zip"
     },
 
     {
       name: "pdfValcea",
       type: "file",
-      title: "Program Valcea .PDF",
+      title: "Program Vâlcea .PDF",
       options: { accept: '.pdf' }
     },
 
      {
       name: "mediaKitValcea",
       type: "file",
-      title: "Media Kit Valcea",
-      description: "Un singur fisier .zip"
+      title: "Media Kit Vâlcea",
+      description: "Un singur fișier .zip"
     },
 
 
@@ -110,11 +110,11 @@ const generalInfo = {
               { name: 'image', title: 'Image', type: 'image', options: { hotspot: true } },
               { name: 'name', title: 'Nume', type: 'string' },
               { name: 'role', title: 'Rol', type: 'string' },
-              { name: 'city', title: 'Oras', type: 'string', initialValue: 'sibiu', options: {
+              { name: 'city', title: 'Oraș', type: 'string', initialValue: 'sibiu', options: {
                           list: [
                             { title: 'Sibiu', value: 'sibiu' },
-                            { title: 'Valcea', value: 'valcea' },
-                            { title: 'Sibiu și Valcea', value: 'sibiu-valcea' },]}
+                            { title: 'Vâlcea', value: 'valcea' },
+                            { title: 'Sibiu și Vâlcea', value: 'sibiu-valcea' },]}
           }]
      }],
     },
@@ -128,13 +128,13 @@ const generalInfo = {
             type: 'object',
             fields: [
               { name: 'name', title: 'Nume', type: 'string' },
-               { name: 'logo', title: 'Logo', type: 'image', options: { hotspot: true }, description:"abordare recomandata: logoul inscris intrun .png 200x100 px" },
+               { name: 'logo', title: 'Logo', type: 'image', options: { hotspot: true }, description:"abordare recomandată: logoul înscris într-un .png 200x100 px" },
               { name: 'type', title: 'Tip', type: 'string', initialValue: 'partner',
                         options: {
                           list: [
                             { title: 'Doar Partener', value: 'partner' },
-                            { title: 'Partner Mobilitate', value: 'mobility-partner' },
-                            { title: 'Partner Media', value: 'media-partner' },
+                            { title: 'Partener Mobilitate', value: 'mobility-partner' },
+                            { title: 'Partener Media', value: 'media-partner' },
                             { title: 'Sponsor', value: 'sponsor' },
                             // Add more options here
                           ] }
@@ -154,10 +154,10 @@ const generalInfo = {
             type: 'object',
             fields: [
               { name: 'name', title: 'Nume', type: 'string', hidden: true },
-              { name: 'city', title: 'Oras', type: 'string', hidden: true, initialValue: 'sibiu', options: {
+              { name: 'city', title: 'Oraș', type: 'string', hidden: true, initialValue: 'sibiu', options: {
                           list: [
                             { title: 'Sibiu', value: 'sibiu' },
-                            { title: 'Valcea', value: 'valcea' },
+                            { title: 'Vâlcea', value: 'valcea' },
                           ] }, validation: (Rule) => Rule.required(), },
               { name: 'link', title: 'Link social media', type: 'string' },
             ],
@@ -180,7 +180,7 @@ const generalInfo = {
 
     defineField({
       name: 'contactFields',
-      title: 'Detalii Contact',
+      title: 'Detalii de Contact',
       type: "array",
       of: [{type: "contactFields"}],
     }),
@@ -188,36 +188,36 @@ const generalInfo = {
     //Linkuri Formulare///////////////
      defineField({
       name: 'externalFormLinks_sibiu',
-      title: 'Linkuri Forumlare Externe - Sibiu',
+      title: 'Linkuri Formulare Externe - Sibiu',
       type: "externalFormLinks",
     }),
 
      defineField({
       name: 'externalFormLinks_valcea',
-      title: 'Linkuri Forumlare Externe - Valcea',
+      title: 'Linkuri Formulare Externe - Vâlcea',
       type: "externalFormLinks",
     }),
 
    //Denumire Sectiuni///////////////
     {
         name: 'sectionNames',
-        title: 'Denumire Sectiuni (separate pe editie)',
+        title: 'Denumire Secțiuni (separate pe ediție)',
         type: 'array',
         of: [
           {
           type: 'object',
           fields: [
             
-            { name: 'year', title: 'Editie', type: 'string' },
-            { name: 's1_sibiu', title: 'Sectiune 1 (Sibiu)', type: 'string' },
-            { name: 's2_sibiu', title: 'Sectiune 2 (Sibiu)', type: 'string' },
-            { name: 's3_sibiu', title: 'Sectiune 3 (Sibiu)', type: 'string' },
-            { name: 's4_sibiu', title: 'Sectiune 4 (Sibiu)', type: 'string' },
+            { name: 'year', title: 'Ediție', type: 'string' },
+            { name: 's1_sibiu', title: 'Secțiune 1 (Sibiu)', type: 'string' },
+            { name: 's2_sibiu', title: 'Secțiune 2 (Sibiu)', type: 'string' },
+            { name: 's3_sibiu', title: 'Secțiune 3 (Sibiu)', type: 'string' },
+            { name: 's4_sibiu', title: 'Secțiune 4 (Sibiu)', type: 'string' },
 
-            { name: 's1_valcea', title: 'Sectiune 1 (Valcea)', type: 'string' },
-            { name: 's2_valcea', title: 'Sectiune 2 (Valcea)', type: 'string' },
-            { name: 's3_valcea', title: 'Sectiune 3 (Valcea)', type: 'string' },
-            { name: 's4_valcea', title: 'Sectiune 4 (Valcea)', type: 'string' },
+            { name: 's1_valcea', title: 'Secțiune 1 (Vâlcea)', type: 'string' },
+            { name: 's2_valcea', title: 'Secțiune 2 (Vâlcea)', type: 'string' },
+            { name: 's3_valcea', title: 'Secțiune 3 (Vâlcea)', type: 'string' },
+            { name: 's4_valcea', title: 'Secțiune 4 (Vâlcea)', type: 'string' },
 
           ],
           components: {
@@ -228,9 +228,9 @@ const generalInfo = {
 
     defineField({
       name: 'currentYear',
-      title: 'Editia activa:',
+      title: 'Ediția activă:',
       type: 'string',
-      description: "Pe website se vor afisa proiecte/evenimente/tururi din editia selectata aici",
+      description: "Pe website se vor afișa proiecte/evenimente/tururi din ediția selectată aici. Arhiva va include edițiile anterioare și inclusiv cea activă (selectată aici)",
       initialValue: (new Date().getFullYear()).toString(),
       validation: (rule) => rule.required().error("O valoare e musai"),
       options: {
@@ -239,7 +239,7 @@ const generalInfo = {
 
       defineField({
         name: "signupForms_areActive",
-        title: "Formulare de inscriere active",
+        title: "Formulare de înscriere active",
         type: "boolean",
         initialValue: true,
       }),
